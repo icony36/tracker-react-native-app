4aaa00d308dc2ddff0e2cb0ee550dd5bb724b58b
icony36 <icode67658@gmail.com>
2019-11-20 14:15:47 +0800

    refactoring

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { AsyncStorage } from "react-native";
 
 const instance = axios.create({
-  baseURL: "http://da53247e.ngrok.io"
+  baseURL: "http://4b510bbf.ngrok.io"
 });
 
 instance.interceptors.request.use(
